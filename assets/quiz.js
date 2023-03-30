fd0bdd8e2902be7e9305c74bd59f5e132524a875
hiroro <hiroaki.a1989@gmail.com>
2023-03-30 17:18:28 -0400

    added quiz

---
+++ assets/quiz.js
@@ -0,0 +1,102 @@
+var quiz = [
+    {
+        question: "What does HTML stand for?",
+        choices: ["HyperText Markup Language", "HyperText Makeup Language", "Home Made Language"],
+        answer: "HyperText Markup Language",
+    },
+    {
+        question: "What is the correct HTML element for inserting a line break? ",
+        choices: ["<br>", "<li>", "<break>"],
+        answer: "<br>",
+    },
+    {
+        question: "What does css stand for?",
+        choices: ["Cascading System Style","Cascading Style Sheet","Cascading style System"],
+        answer: "Cascading Style Sheet",
+    },
+    {
+        question: "Where in an HTML document is the correct place to refer to an external style sheet? ",
+        choices: ["At the end of document", "body section", "head section" ],
+        answer: "head section",
+    },
+    {
+        question: "Which HTML tag is used to define an internal style sheet?",
+        choices: ["<css>", "<script>", "<style>"],
+        answer: "<style>",
+    },
+    {
+        question: "How do you insert a comment in a CSS file?",
+        choices: ["/* */","//","<-! -!>"],
+        answer: "/* */",
+    },
+    {
+        question: "Which property is used to change the font color?",
+        choices: ["background-color", "color", "bg-color"],
+        answer: "color",
+    },
+    {
+        question: "What does DOM stand for?",
+        choices: ["Document Object Model", " Domestic Object Model", "Dog Oriented Model"],
+        answer: "Document Object Model",
+    },
+    {
+        question: "Where is the best place to insert an external JavaScript?",
+        choices: ["inside of <body>", "inside of <head>", "inside of <header>"],
+        answer: "inside of <body>",
+    },
+    {
+        question: "What is the correct syntax of function expression below?",
+        choices: ["var myFunction = function(){};", " function myFunction(){};", "var = function(){};"],
+        answer: "var myFunction = function(){};",
+    },
+    {
+        question:"How many percent of websites is the JavaScript used on the client side?",
+        choices: ["50%", "74%", "98%"],
+        answer: "98%",
+    },
+    {
+        question: "What does API stand for?",
+        choices: ["Apple Program Interface ", "Application Programming Interface", "Agile Program Interface"],
+        answer: "Application Programming Interface",
+    },
+    {
+        question: "What does DRY stand for?",
+        choices: ["Do Reapeat Yourself", "Don't Reapeat Yourself", "Do Respect Yourself"],
+        answer: "Don't Reapeat Yourself",
+    },
+    {
+        question: "The # symbol specifies that the selector is?"
+        choices: ["id", "hashtag", "class"],
+        answer: "id",
+    },
+    {
+        question: "What is strictly equal operator? ",
+        choices: ["=","==", "==="],
+        answer: "===",
+    },
+    {
+        question: "What is correct for roop syntax?",
+        choices: ["for(var i =0, i<5, i++)","for var i=0; i-1; i++", "for(var i=0; i<5; i++"],
+        answer: "for(var i=0; i<5; i++",
+    },
+    {
+        question: "what is the commment in JavaScrip?",
+        choices: ["//","/* */", "<!-- -->"],
+        answer: "//",
+    },
+    {
+        question: "How do you declare a JS variable?",
+        choices: ["V vName","var vName", "vari vName"],
+        answer: "var vName",
+    },
+    {
+        question: "What is modulus operator in JS?",
+        choices: ["=","+", "%"],
+        answer: "%",
+    },
+    {
+        question: "What is a semantic element below? ",
+        choices: ["<header>","<h1>", "<button>"],
+        answer: "<header>",
+    },
+]
